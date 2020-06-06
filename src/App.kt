1c0118e8d3948aba22979d3285720a7eb8dbdedb
shibamoto <silverbirder@gmail.com>
2020-06-06 23:42:34 +0900

    👨‍💻 Add sample code

--- src/App.kt
+++ src/App.kt
@@ -1,3 +1,22 @@
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun describe(obj: Any): String =
+    when (obj) {
+        1 -> "One"
+        "Hello" -> "Greeting"
+        else -> "Unknown"
+    }
+
+data class Customer(val name: String = "default", val email: String)
+
 fun main() {
-    println("Hello World!")
+    val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+    fruits
+        .filter { it.startsWith("a") }
+        .sortedBy { it }
+        .map { it.toUpperCase() }
+        .forEach { println(it) }
+    var c = Customer(email = "name@email.com")
 }
