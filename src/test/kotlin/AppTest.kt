fb79ddd8e560c1f52ef86c9fe124529fb9897eea
shibamoto <silverbirder@gmail.com>
2020-06-22 07:23:47 +0900

    🛠 Add test code of my product code by kotest

--- src/test/kotlin/AppTest.kt
+++ src/test/kotlin/AppTest.kt
@@ -1,14 +1,10 @@
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.inspectors.forExactly
-import io.kotest.matchers.string.shouldContain
-import io.kotest.matchers.string.shouldStartWith
+import io.kotest.matchers.shouldBe
+import main.kotlin.sum
 
 class MyTests : StringSpec({
-    "your test case" {
-        val xs = listOf("aa_1", "aa_2", "aa_3")
-        xs.forExactly(3) {
-            it.shouldContain("_")
-            it.shouldStartWith("aa")
-        }
+    "sum case" {
+        val result = sum(1,2)
+        result.shouldBe(3)
     }
 })
