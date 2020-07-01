ed0b2629c45a328a451f45328e8e1a30f614ff2d
shibamoto <silverbirder@gmail.com>
2020-07-02 07:57:37 +0900

    Try Partition

--- src/main/kotlin/Bitfyer.kt
+++ src/main/kotlin/Bitfyer.kt
@@ -1,14 +1,17 @@
 package main.kotlin
 
+// @see https://gist.github.com/marcoslin/e1e19afdbacac9757f6974592cfd8d7f#file-working-kt-L10
+
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.io.TextIO
 import org.apache.beam.sdk.options.Default
 import org.apache.beam.sdk.options.PipelineOptions
 import org.apache.beam.sdk.options.PipelineOptionsFactory
 import org.apache.beam.sdk.transforms.*
-import org.apache.beam.sdk.values.*
-
-// @see https://gist.github.com/marcoslin/e1e19afdbacac9757f6974592cfd8d7f#file-working-kt-L10
+import org.apache.beam.sdk.transforms.Partition.PartitionFn
+import org.apache.beam.sdk.values.KV
+import org.apache.beam.sdk.values.PCollection
+import org.apache.beam.sdk.values.PCollectionList
 import java.lang.Iterable as JavaIterable
 
 
@@ -27,12 +30,19 @@ object Bitfyer {
     class FilterBit : PTransform<PCollection<String>, PCollection<KV<String, Int>>>() {
         override fun expand(input: PCollection<String>): PCollection<KV<String, Int>>? {
             var a = input.apply(ParDo.of(Extract()))
-            var pc = PCollectionList.of(a).and(a)
-            var pa = pc.apply(Flatten.pCollections())
+            var pp = a.apply(Partition.of(10, PartitionFunc()))
+//            var pc = PCollectionList.of(a).and(a)
+            var pa = pp.apply(Flatten.pCollections())
 //            var b = a.apply(GroupByKey.create<String, Int>()) as PCollection<KV<String, JavaIterable<Int>>>
             var d = pa.apply(Sum.integersPerKey())
             var u = d.apply<PCollection<KV<String, Int>>>(Combine.perKey(SumInt()))
             return u
+        }
+    }
+
+    internal class PartitionFunc: Partition.PartitionFn<KV<String, Int>> {
+        override fun partitionFor(elem: KV<String, Int>, numPartitions: Int): Int {
+            return numPartitions / 100
         }
     }
 
