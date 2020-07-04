de51453811da0ced43b58ee9e01bf50fd1e01148
shibamoto <silverbirder@gmail.com>
2020-07-04 16:01:28 +0900

    Transform the JSON object

--- src/main/kotlin/Oanda.kt
+++ src/main/kotlin/Oanda.kt
@@ -21,6 +21,20 @@ data class OandaJson(
     val candles: List<Candlestick> = listOf()
 )
 
+@DefaultCoder(AvroCoder::class)
+data class FlattenOandaJson(
+    val instrument: String = "",
+    val granularity: Granularity = Granularity.S5,
+    val type: String = "",
+    val time: String = "",
+    val volume: Int = 0,
+    val complete: Boolean = false,
+    val o: String = "",
+    val h: String = "",
+    val l: String = "",
+    val c: String = ""
+)
+
 enum class Granularity {
     S5, S10, S15, S30, M1, M2, M4, M5, M10, M15, M30, H1, H2, H3, H4, H6, H8, H12, D, W, M
 }
@@ -54,9 +68,11 @@ object Oanda {
         p.run().waitUntilFinish()
     }
 
-    internal class Transform : PTransform<PCollection<String>, PCollection<KV<String, OandaJson>>>() {
-        override fun expand(input: PCollection<String>): PCollection<KV<String, OandaJson>> {
-            return input.apply(ParDo.of(JsonToData()))
+    internal class Transform : PTransform<PCollection<String>, PCollection<KV<String, FlattenOandaJson>>>() {
+        override fun expand(input: PCollection<String>): PCollection<KV<String, FlattenOandaJson>> {
+            return input
+                .apply(ParDo.of(JsonToData()))
+                .apply(ParDo.of(FlattenCandles()))
         }
     }
 
@@ -69,8 +85,35 @@ object Oanda {
         }
     }
 
-    internal class Format : SimpleFunction<KV<String, OandaJson>, String>() {
-        override fun apply(input: KV<String, OandaJson>): String {
+    internal class FlattenCandles : DoFn<KV<String, OandaJson>, KV<String, FlattenOandaJson>>() {
+        @ProcessElement
+        fun processElement(c: ProcessContext) {
+            val el = c.element()
+            val oanda = el.value
+            val candles = oanda.candles
+            val flattenOanda = mutableListOf<FlattenOandaJson>()
+            candles.map {
+                flattenOanda.add(
+                    FlattenOandaJson(
+                        instrument = oanda.instrument,
+                        granularity = oanda.granularity,
+                        type = "",
+                        time = it.time,
+                        volume = it.volume,
+                        complete = it.complete,
+                        o = "",
+                        h = "",
+                        l = "",
+                        c = ""
+                    )
+                )
+            }
+            c.output(KV.of(el.key, flattenOanda[0]))
+        }
+    }
+
+    internal class Format : SimpleFunction<KV<String, FlattenOandaJson>, String>() {
+        override fun apply(input: KV<String, FlattenOandaJson>): String {
             return "" + input.key
         }
     }
