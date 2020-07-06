0c77845ed146c1993d00c3a2ea12d887f46ed6e5
shibamoto <silverbirder@gmail.com>
2020-07-07 07:21:59 +0900

    😎 Pass the test code

--- src/test/kotlin/OandaTest.kt
+++ src/test/kotlin/OandaTest.kt
@@ -1,7 +1,7 @@
 package test.kotlin
 
-import io.kotest.matchers.nulls.shouldNotBeNull
 import main.kotlin.FlattenOandaJson
+import main.kotlin.Granularity
 import main.kotlin.Oanda.Transform
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.testing.PAssert
@@ -17,18 +17,32 @@ class OandaTest {
     fun oandaTransformTest() {
         // Arrange
         val p: Pipeline = TestPipeline.create().enableAbandonedNodeEnforcement(false)
-        val input: PCollection<String> = p.apply(Create.of(""))
+        val input: PCollection<String> = p.apply(Create.of(INPUT))
         val output: PCollection<KV<String, FlattenOandaJson>> = input.apply(Transform())
 
         // Act
         p.run()
 
         // Assert
-        PAssert.that<KV<String, FlattenOandaJson>>(output).shouldNotBeNull()
+        PAssert.that<KV<String, FlattenOandaJson>>(output).containsInAnyOrder(OUTPUT)
     }
 
     companion object {
-        private val f = FlattenOandaJson()
-        val INPUT: KV<String, FlattenOandaJson> = KV.of("a", f)
+        const val INPUT: String =
+            "{\"instrument\":\"USD_JPY\",\"granularity\":\"S5\",\"candles\":[{\"complete\":true,\"volume\":1,\"time\":\"2020-07-03T10:15:45.000000000Z\",\"bid\":{\"o\":\"107.492\",\"h\":\"107.492\",\"l\":\"107.492\",\"c\":\"107.492\"}}]}"
+        val OUTPUT: KV<String, FlattenOandaJson> = KV.of(
+            "USD_JPY", FlattenOandaJson(
+                instrument = "USD_JPY",
+                granularity = Granularity.S5,
+                complete = true,
+                volume = 1,
+                time = "2020-07-03T10:15:45.000000000Z",
+                type = "bid",
+                o = 107.492,
+                h = 107.492,
+                l = 107.492,
+                c = 107.492
+            )
+        )
     }
 }
